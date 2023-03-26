b70c941d2183ff52b5c548aec9330c9d9f9e881b
ZorsCode <zhosyuy21@gmail.com>
2023-03-26 19:12:23 +0800

    Fixed responsiveness on each card, used card, card-body,card text for each section. Modified content to ensure clarity, created a modal for contact button and save information function for the captured data.

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 const navLink = document.querySelectorAll(`.nav-link`);
 const secTion = document.querySelectorAll(`section`);
 
-
-
 function activeMenu (){
   let leng = secTion.length;
   while (--leng && window.scrollY +97 < secTion[leng].offsetTop){}
@@ -12,3 +10,30 @@ function activeMenu (){
 
 activeMenu();
 window.addEventListener(`scroll`,activeMenu);
+
+let modalInfo =[];
+
+const modalName = document.getElementById(`modalName`);
+const modalEmail = document.getElementById(`modalEmail`);
+const modalText = document.getElementById(`modalText`);
+
+function captureModal(){
+
+  let contactInfo = {
+    modalName :  modalName.value,
+    modalEmail :  modalEmail.value,
+    modalText :  modalText.value
+  };
+
+  let storedContacts = localStorage.getItem(contactInfo.modalEmail);
+
+  let contactForms = storedContacts ? JSON.parse(storedContacts) : []; 
+  contactForms.push(contactInfo);
+
+  localStorage.setItem(`Contact`, JSON.stringify(contactForms));
+}
+
+window.onload = function (){
+  let contactForm = document.getElementById(`contactForm`);
+  contactForm.onsubmit = captureModal;
+};
